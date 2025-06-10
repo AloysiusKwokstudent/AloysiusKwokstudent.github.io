ea93553eccebcc8cd141d17997d3096280cba838
AzureWorks78 <azureworks78@gmail.com>
2025-06-11 04:40:42 +0900

    Upload new folder

---
+++ class-changes.js
@@ -0,0 +1,55 @@
+const classChanges = [
+  { time: 24, target: ".para1", add: "active" },
+  { time: 34, target: ".para1", remove: "active" },
+
+  { time: 81, target: ".para2", add: "active" },
+  { time: 92, target: ".para2", remove: "active" },
+
+  { time: 143, target: ".para3", add: "active" },
+  { time: 159, target: ".para3", remove: "active" },
+
+  { time: 186, target: ".para4", add: "active" },
+  { time: 194, target: ".para4", remove: "active" },
+
+  { time: 213, target: ".para5", add: "active" },
+  { time: 216, target: ".para5", remove: "active" },
+
+  { time: 233, target: ".para6", add: "active" },
+  { time: 235, target: ".para6", remove: "active" },
+
+  { time: 246, target: ".para7", add: "active" },
+  { time: 248, target: ".para7", remove: "active" },
+
+  { time: 262, target: ".para8", add: "active" },
+  { time: 265, target: ".para8", remove: "active" },
+
+  { time: 277, target: ".para9", add: "active" },
+  { time: 284, target: ".para9", remove: "active" },
+
+  { time: 289, target: ".para10", add: "active" },
+  { time: 284, target: ".para10", remove: "active" },
+
+  { time: 323, target: ".para11", add: "active" },
+  { time: 326, target: ".para11", remove: "active" },
+
+  { time: 350, target: ".para12", add: "active" },
+  { time: 354, target: ".para12", remove: "active" },
+
+  { time: 358, target: ".para13", add: "active" },
+  { time: 368, target: ".para13", remove: "active" },
+
+
+
+
+
+
+  { time: 9, target: ".changeme", add: "hidden" },
+
+  { time: 9, target: ".changeme2", add: "show" },
+  { time: 16, target: ".changeme2", remove: "show" },
+  { time: 16, target: ".changeme2", add: "hidden" },
+
+  { time: 16, target: ".changeme3", add: "show" },
+  { time: 28, target: ".changeme3", remove: "show" },
+  { time: 28, target: ".changeme3", add: "hidden" }
+];
